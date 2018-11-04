6fadfb4e409700430f3bd1124495e72b4f50717c
leozoge <ldc.monteiro@yahoo.com.br>
2018-11-04 03:29:54 -0200

    Simple mathematical operations calculator

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,15 +6,15 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first digit: (Digite o primeiro algarismo)");
+            Console.WriteLine("Enter the first digit (Digite o primeiro algarismo): ");
             string firstDigitInString = Console.ReadLine();
             decimal firstDigitInDecimal = Convert.ToDecimal(firstDigitInString);
 
-            Console.WriteLine("Enter the first digit: (Digite o segundo algarismo)");
+            Console.WriteLine("Enter the first digit (Digite o segundo algarismo): ");
             string secondDigitInString = Console.ReadLine();
             decimal secondDigitInDecimal = Convert.ToDecimal(secondDigitInString);
 
-            Console.WriteLine("Enter the operation: (Digite a operação)");
+            Console.WriteLine("Enter the operation (Digite a operação): ");
             string operation = Console.ReadLine();
 
             decimal result = 0;
@@ -38,19 +38,36 @@ namespace Calculadora
             }
             else
             {
-                Console.WriteLine("Wrong operation! (Operação errada!)");
+                Console.WriteLine("Wrong operation! (Operação errada!) ");
             }
 
-            Console.WriteLine("The result is: (O resultado foi)" + result);
+            Console.WriteLine("The result is (O resultado foi): " + result);
+
             Console.WriteLine();
+
             decimal div = firstDigitInDecimal / secondDigitInDecimal;
+
             decimal rest = firstDigitInDecimal % secondDigitInDecimal;
-            Console.WriteLine("The rest is: (O resto é)" + rest);
-            Console.WriteLine();
-            if (result % 2 == 0)
-                Console.WriteLine("So it's PAR (Logo é PAR)");
+
+            if (rest != 0)
+            {
+                Console.WriteLine("The rest is (O resto é): " + rest);
+            }
             else
-                Console.WriteLine("So it's ODD (Logo é íMPAR)");
+            {
+                Console.WriteLine("The rest is (O resto é): 0");
+            }
+
+            Console.WriteLine();
+
+            if (result % 2 == 0)
+            {
+                Console.WriteLine(" So it's PAR (Logo é PAR)");
+            }
+            else
+            {
+                Console.WriteLine(" So it's ODD (Logo é íMPAR)");
+            }
 
             Console.ReadKey();
         }
