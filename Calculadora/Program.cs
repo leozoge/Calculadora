b9460d07d72577df8af17f8acbf4f65d4b4e2dba
leozoge <ldc.monteiro@yahoo.com.br>
2018-11-04 03:14:09 -0200

    simple mathematical operations calculator

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -41,16 +41,16 @@ namespace Calculadora
                 Console.WriteLine("Wrong operation! (Operação errada!)");
             }
 
-            Console.WriteLine("O resultado foi: " + result);
+            Console.WriteLine("The result is: (O resultado foi)" + result);
             Console.WriteLine();
             decimal div = firstDigitInDecimal / secondDigitInDecimal;
-            decimal resto = firstDigitInDecimal % secondDigitInDecimal;
-            Console.WriteLine("O resto é = " + resto);
+            decimal rest = firstDigitInDecimal % secondDigitInDecimal;
+            Console.WriteLine("The rest is: (O resto é)" + rest);
             Console.WriteLine();
             if (result % 2 == 0)
-                Console.WriteLine("So it's par (Logo é PAR)");
+                Console.WriteLine("So it's PAR (Logo é PAR)");
             else
-                Console.WriteLine("So it's odd (Logo é íMPAR)");
+                Console.WriteLine("So it's ODD (Logo é íMPAR)");
 
             Console.ReadKey();
         }
